2d4b8e6e6928a432ba35435a69c0ad841d3ce1d8
rijoroy <30549640+rijoroy@users.noreply.github.com>
2017-08-22 03:27:16 -0700

    Create number.c

---
+++ number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if (a>0)
+printf("the number is positive");
+else if(a<0)
+printf("the number is negative");
+else
+printf("the number is zero");
+getch();
+}
